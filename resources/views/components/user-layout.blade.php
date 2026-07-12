ae312d0d33a3ce68f141844ef12c88e9c8e6e554
mat.isra <mat.isra99@gmail.com>
2026-07-12 20:31:49 +0700

    update: fitur statistik dan perbaikan menu

--- resources/views/components/user-layout.blade.php
+++ resources/views/components/user-layout.blade.php
@@ -52,6 +52,17 @@
             </a>
         </li>
         <li>
+            @php
+                $statUrl = $litProject ? route('statistics.index', $litProject) : route('user.projects.index');
+            @endphp
+            <a href="{{ $statUrl }}" class="group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold {{ request()->routeIs('statistics.*') ? 'bg-gray-50 text-fuchsia-600' : 'text-gray-700 hover:bg-gray-50 hover:text-fuchsia-600' }}">
+                <svg class="h-6 w-6 shrink-0 {{ request()->routeIs('statistics.*') ? 'text-fuchsia-600' : 'text-gray-400 group-hover:text-fuchsia-600' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                </svg>
+                Analisis Statistik
+            </a>
+        </li>
+        <li>
             <a href="{{ route('user.subscription.index') }}" class="group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold {{ request()->routeIs('user.subscription.*') ? 'bg-gray-50 text-amber-600' : 'text-gray-700 hover:bg-gray-50 hover:text-amber-600' }}">
                 <svg class="h-6 w-6 shrink-0 {{ request()->routeIs('user.subscription.*') ? 'text-amber-600' : 'text-gray-400 group-hover:text-amber-600' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
